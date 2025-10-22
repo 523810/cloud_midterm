6e5a59abf1b082edbbb89e2eaac853949b3eac10
523810 <damenke15@gmail.com>
2025-10-22 11:51:34 +0900

    실기1 수정

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@
 #
 
 
-from flask import Flask  
+from flask import Flask, request, jsonify, send_file
 from pathlib import Path
 import json, os
 
@@ -26,14 +26,63 @@ def healthz():
 # @app.get("/api/records")
 # def get_records():
 #     raise NotImplementedError
+@app.get("/api/records")
+def get_records():
+    json_text = DATA_PATH.read_text(encoding="utf-8")
+
+    if not json_text:
+        return jsonify([]), 200
+    
+    data = json.loads(json_text)
+    return jsonify(data), 200
 
 # @app.post("/api/records")
 # def add_record():
 #     raise NotImplementedError
+@app.post("/api/records")
+def add_record():
+    payload = request.get_json()
 
+    if not payload:
+        return jsonify({"error": "JSON 데이터가 비어있습니다."}), 400
+
+    height = payload.get("height")
+    weight = payload.get("weight")
+
+    if not height or not weight:
+         return jsonify({"error": "'height'와 'weight' 값은 필수입니다."}), 400  
+    if not (isinstance(height, (int, float)) and height > 0):
+         return jsonify({"error": "'height'는 0보다 큰 숫자여야 합니다."}), 400
+    if not (isinstance(weight, (int, float)) and weight > 0):
+         return jsonify({"error": "'weight'는 0보다 큰 숫자여야 합니다."}), 400
+
+    new_record = {"height": height, "weight": weight}
+
+    current_data = []
+    json_text = DATA_PATH.read_text(encoding="utf-8")
+
+    if json_text:
+        current_data = json.loads(json_text)
+
+        if not isinstance(current_data, list):
+            currnet_data = []
+
+    current_data.append(new_record)
+
+    DATA_PATH.write_text(json.dumps(current_data, indent=2, ensure_ascii=False), encoding="utf-8")
+
+    return jsonify(new_record), 201
 # @app.get("/api/download")
 # def download_json():
 #     raise NotImplementedError
+@app.get("/api/download")
+def download_json():
+    
+    return send_file(
+        DATA_PATH,
+        as_attachment=True,
+        download_name="records.json"
+    )
 
 if __name__ == "__main__":
     # 적절한 포트(예: 5000)로 0.0.0.0 에서 실행
